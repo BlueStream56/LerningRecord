6156cef0866c8d1a7a23d2395db5f23b42f2c3c7
BlueStream56 <453663721@qq.com>
2020-03-29 22:31:59 +0800

    命令模式

---
+++ DesignPatterns/src/main/java/com/example/designpatterns/behaviour/command/Broker.java
@@ -0,0 +1,21 @@
+package com.example.designpatterns.behaviour.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Broker {
+
+    private List<Order> orderList = new ArrayList<Order>();
+
+    public void takeOrder(Order order){
+        orderList.add(order);
+    }
+
+    public void placeOrders(){
+        orderList.stream().forEach(order -> {
+            order.execute();
+        });
+        orderList.clear();
+    }
+
+}
---
+++ DesignPatterns/src/main/java/com/example/designpatterns/behaviour/command/SellStock.java
@@ -0,0 +1,15 @@
+package com.example.designpatterns.behaviour.command;
+
+public class SellStock implements Order {
+
+    private Stock abcStock;
+
+    public SellStock(Stock abcStock) {
+        this.abcStock = abcStock;
+    }
+
+    @Override
+    public void execute() {
+        abcStock.sell();
+    }
+}
