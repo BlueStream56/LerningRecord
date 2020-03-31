667c3591378168adf4ac92146cb78a8bf4becd29
caoxx <453663721@qq.com>
2020-03-31 16:51:59 +0800

    前端控制器模式

---
+++ DesignPatterns/src/main/java/com/example/designpatterns/j2ee/frontcontroller/FrontController.java
@@ -0,0 +1,49 @@
+package com.example.designpatterns.j2ee.frontcontroller;
+
+/**
+ * <ul>
+ * <li>文件名称：FrontController </li>
+ * <li>文件描述：暂无描述</li>
+ * <li>版权所有：版权所有(C) 2016</li>
+ * <li>公 司：厦门云顶伟业信息技术有限公司</li>
+ * <li>内容摘要：</li>
+ * <li>其他说明：</li>
+ * <li>创建日期：2020/3/31 </li>
+ * </ul>
+ *
+ * <ul>
+ * <li>修改记录：</li>
+ * <li>版 本 号：</li>
+ * <li>修改日期：</li>
+ * <li>修 改 人：</li>
+ * <li>修改内容：</li>
+ * </ul>
+ *
+ * @author caoxx
+ * @version 1.0.0
+ */
+public class FrontController {
+
+    private Dispatcher dispatcher;
+
+    public FrontController() {
+        dispatcher = new Dispatcher();
+    }
+
+    private boolean isAuthenticUser(){
+        System.out.println("User is authenticated successfully.");
+        return true;
+    }
+
+    private void trackRequest(String request){
+        System.out.println("Page requested: " + request);
+    }
+
+    public void dispatchRequest(String requset){
+        trackRequest(requset);
+        if (isAuthenticUser()){
+            dispatcher.dispatch(requset);
+        }
+    }
+
+}
