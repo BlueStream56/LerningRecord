e01903793f178cf62a952710020b7b52af24d094
caoxx <453663721@qq.com>
2020-03-31 14:28:59 +0800

    访问者模式

---
+++ DesignPatterns/src/main/java/com/example/designpatterns/behaviour/visitor/ComputerPartDisplayVisitor.java
@@ -0,0 +1,45 @@
+package com.example.designpatterns.behaviour.visitor;
+
+/**
+ * <ul>
+ * <li>文件名称：ComputerPartDisplayVisitor </li>
+ * <li>文件描述：暂无描述</li>
+ * <li>版权所有：版权所有(C) 2016</li>
+ * <li>公 司：厦门云顶伟业信息技术有限公司</li>
+ * <li>内容摘要：</li>
+ * <li>其他说明：</li>
+ * <li>创建日期：2020/3/31 </li>
+ * </ul>
+ *
+ * <ul>
+ * <li>修改记录：</li>
+ * <li>版 本 号：</li>
+ * <li>修改日期：</li>
+ * <li>修 改 人：</li>
+ * <li>修改内容：</li>
+ * </ul>
+ *
+ * @author caoxx
+ * @version 1.0.0
+ */
+public class ComputerPartDisplayVisitor implements ComputerPartVisitor {
+    @Override
+    public void visit(Computer computer) {
+        System.out.println("Displaying Computer.");
+    }
+
+    @Override
+    public void visit(Mouse mouse) {
+        System.out.println("Displaying Mouse.");
+    }
+
+    @Override
+    public void visit(Keyboard keyboard) {
+        System.out.println("Displaying Keyboard.");
+    }
+
+    @Override
+    public void visit(Monitor monitor) {
+        System.out.println("Displaying Monitor.");
+    }
+}
