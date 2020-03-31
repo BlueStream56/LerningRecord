19e8a2a226fefe48797ce353bfd2eda59132f338
caoxx <453663721@qq.com>
2020-03-31 09:32:08 +0800

    空对象模式

---
+++ DesignPatterns/src/main/java/com/example/designpatterns/behaviour/nullobject/NullPatternDemo.java
@@ -0,0 +1,40 @@
+package com.example.designpatterns.behaviour.nullobject;
+
+/**
+ * <ul>
+ * <li>文件名称：NullPatternDemo </li>
+ * <li>文件描述：暂无描述</li>
+ * <li>版权所有：版权所有(C) 2016</li>
+ * <li>公 司：厦门云顶伟业信息技术有限公司</li>
+ * <li>内容摘要：</li>
+ * <li>其他说明：</li>
+ * <li>创建日期：2020/3/31 </li>
+ * </ul>
+ *
+ * <ul>
+ * <li>修改记录：</li>
+ * <li>版 本 号：</li>
+ * <li>修改日期：</li>
+ * <li>修 改 人：</li>
+ * <li>修改内容：</li>
+ * </ul>
+ *
+ * @author caoxx
+ * @version 1.0.0
+ */
+public class NullPatternDemo {
+
+    public static void main(String[] args) {
+        AbstractCustomer customer1 = CustomerFactory.getCustomer("Rob");
+        AbstractCustomer customer2 = CustomerFactory.getCustomer("Bob");
+        AbstractCustomer customer3 = CustomerFactory.getCustomer("Julie");
+        AbstractCustomer customer4 = CustomerFactory.getCustomer("Laura");
+
+        System.out.println("Customers");
+        System.out.println(customer1.getName());
+        System.out.println(customer2.getName());
+        System.out.println(customer3.getName());
+        System.out.println(customer4.getName());
+    }
+
+}
